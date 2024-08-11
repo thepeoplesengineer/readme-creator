5301c5c6580b509e03ab117e188834b9d5b7ea3c
Sammy Kordi <Sammy.Kordi@gmail.com>
2024-08-11 15:54:52 -0700

    Updated license selector to post links

--- index.js
+++ index.js
@@ -1,77 +1,75 @@
-// TODO: Include packages needed for this application
 import fs from 'fs';
 import inquirer from 'inquirer';
 import generateMarkdown from './utils/generateMarkdown.js';
 
-// TODO: Create an array of questions for user input
+
 const questions = [
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What is the title of your project?',
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Provide a description of your project:',
-    },
-    {
-      type: 'input',
-      name: 'installation',
-      message: 'Describe the installation process:',
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'Provide instructions on how to use:',
-    },
-    {
-      type: 'input',
-      name: 'license',
-      message: 'Enter the project license:',
-    },
-    {
-      type: 'input',
-      name: 'contributing',
-      message: 'Provide guidelines for contributing:',
-    },
-    {
-      type: 'input',
-      name: 'tests',
-      message: 'Provide instructions for running tests:',
-    },
-    {
-      type: 'input',
-      name: 'questions',
-      message: 'Enter your GitHub username for questions:',
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'Enter your email address for questions:',
-    },
-  ];
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Describe the installation process:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide instructions on how to use:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Select the project license:',
+    choices: ['MIT', 'Apache 2.0', 'GPL', 'BSD', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide guidelines for contributing:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide instructions for running tests:',
+  },
+  {
+    type: 'input',
+    name: 'questions',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address for questions:',
+  },
+];
 
+// Function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error('Error writing README.md:', err);
+    } else {
+      console.log('README.md generated successfully!');
+    }
+  });
+}
 
-// TODO: Create a function to write README file
-function writeToFile(questions) {
-    inquirer.prompt(questions).then((data) => {
-      const readmeContent = generateMarkdown(data);
-  
-      // Write the README file
-      fs.writeFile('README.md', readmeContent, (err) => {
-        if (err) {
-          console.error('Error writing README.md:', err);
-        } else {
-          console.log('README.md generated successfully!');
-        }
-      });
-    });
-  }
-// TODO: Create a function to initialize app
-function init(questions) {
-    writeToFile(questions);
+// Function to initialize the app
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    const readmeContent = generateMarkdown(data);
+    writeToFile('README.md', readmeContent);
+  });
 }
 
 // Function call to initialize app
-init(questions);
+init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,61 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  
+  if (!license) {
+    return '';
+  }
+
+  const licenseBadges = {
+    'MIT': '![MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)',
+    'GPLv3': '![GPLv3 License](https://img.shields.io/badge/License-GPLv3-blue.svg)',
+    'Apache 2.0': '![Apache License](https://img.shields.io/badge/License-Apache%202.0-green.svg)',
+    'BSD 3-Clause': '![BSD 3-Clause License](https://img.shields.io/badge/License-BSD%203--Clause-orange.svg)',
+    'None': '',
+  };
+
+  return licenseBadges[license] || '';
+}
+
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+if (!license) {
+  return '';
+}
+
+const licenseLinks = {
+  'MIT': 'https://opensource.org/licenses/MIT',
+  'GPLv3': 'https://www.gnu.org/licenses/gpl-3.0',
+  'Apache 2.0': 'https://opensource.org/licenses/Apache-2.0',
+  'BSD 3-Clause': 'https://opensource.org/licenses/BSD-3-Clause',
+  'None': '',
+  // Add other licenses as needed
+};
+
+return licenseLinks[license] || '';
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+if (!license) {
+  return '';
+}
+
+return `
+## License
+This application is covered under the [${license}](${renderLicenseLink(license)}) license.
+`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `
+return `
 # ${data.title}
+${renderLicenseBadge(data.license)}
 
 ## Description
 ${data.description}
@@ -33,8 +75,7 @@ ${data.installation}
 ## Usage
 ${data.usage}
 
-## License
-This project is licensed under the ${data.license} license.
+${renderLicenseSection(data.license)}
 
 ## Contributing
 ${data.contributing}
@@ -44,7 +85,9 @@ ${data.tests}
 
 ## Questions
 If you have any questions about the project, you can reach me at [${data.email}](mailto:${data.email}). You can also find more of my work at [${data.questions}](https://github.com/${data.questions}).
-  `;
+`;
 }
 
 export default generateMarkdown;
+
+
